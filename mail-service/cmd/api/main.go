8a850be8ac632dcf2c76c543f6e79391b0b6870a
dsolerh <dsolerh.cinter95@gmail.com>
2022-09-29 20:14:43 +0300

    adding the send route to mail service

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,15 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
+	Mailer Mail
 }
 
 const webPort = "80"
 
 func main() {
-	app := Config{}
+	app := Config{
+		Mailer: createMail(),
+	}
 
 	log.Println("Starting mail service on port", webPort)
 
@@ -24,5 +29,19 @@ func main() {
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	return Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("MAIL_NAME"),
+		FromAddress: os.Getenv("MAIL_ADDRESS"),
 	}
 }
