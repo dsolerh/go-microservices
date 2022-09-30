253506f2c3c72b52089e447958dbc3a79dd4ab8d
dsolerh <dsolerh.cinter95@gmail.com>
2022-09-30 10:09:21 +0300

    update the listener service

---
+++ listener-service/event/consumer.go
@@ -0,0 +1,134 @@
+package event
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn      *amqp.Connection
+	queueName string
+}
+
+func NewConsumer(conn *amqp.Connection) (*Consumer, error) {
+	consumer := &Consumer{
+		conn: conn,
+	}
+
+	err := consumer.setup()
+	if err != nil {
+		return nil, err
+	}
+
+	return consumer, nil
+}
+
+func (c *Consumer) setup() error {
+	channel, err := c.conn.Channel()
+	if err != nil {
+		return err
+	}
+	return declareExchange(channel)
+}
+
+type Payload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
+func (c *Consumer) Listen(topics []string) error {
+	ch, err := c.conn.Channel()
+	if err != nil {
+		return err
+	}
+	defer ch.Close()
+
+	q, err := declareRandomQueue(ch)
+	if err != nil {
+		return err
+	}
+
+	for _, topic := range topics {
+		err = ch.QueueBind(
+			q.Name,
+			topic,
+			"logs_topic",
+			false,
+			nil,
+		)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	messages, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+
+	forever := make(chan bool)
+	go func() {
+		for d := range messages {
+			var payload Payload
+			_ = json.Unmarshal(d.Body, &payload)
+
+			go handlePayload(payload)
+		}
+	}()
+
+	log.Printf("Waiting for message [Exchange, Queue] [logs_topic, %s]\n", q.Name)
+	<-forever
+
+	return nil
+}
+
+func handlePayload(p Payload) {
+	switch p.Name {
+	case "log", "event":
+		// log whatever we get
+		err := logEvent(p)
+		if err != nil {
+			log.Println(err)
+		}
+
+	case "auth":
+		// authenticate
+
+	default:
+		err := logEvent(p)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func logEvent(entry Payload) error {
+
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+	logServiceURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		return errors.New("invalid status")
+	}
+
+	return nil
+}
